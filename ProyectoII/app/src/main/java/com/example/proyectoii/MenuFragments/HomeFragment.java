ac204d69d66483b4abcd0b66fec8eea7381a65fd
Alfredo Campos <joscaes12@gmail.com>
2019-10-24 23:18:55 -0600

    added the posts activity

--- ProyectoII/app/src/main/java/com/example/proyectoii/MenuFragments/HomeFragment.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/MenuFragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.proyectoii.MenuFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.proyectoii.Post;
 import com.example.proyectoii.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
 
@@ -17,6 +20,15 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Post.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
