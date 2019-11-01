b243f865fdf99364fc9daf4b64e8e903bef9ba6a
Alfredo Campos <joscaes12@gmail.com>
2019-10-31 18:20:51 -0600

    post layout done, pending functions that handle post feature

--- ProyectoII/app/src/main/java/com/example/proyectoii/Post.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/Post.java
@@ -3,6 +3,9 @@ package com.example.proyectoii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Post extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class Post extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Button back = findViewById(R.id.btn_post_back);
+        ImageButton addImg = findViewById(R.id.addimagen);
+        ImageButton addvid = findViewById(R.id.addvideourl);
+        Button publishbtn = findViewById(R.id.publicar);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        addImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open gallery to select image
+            }
+        });
+
+        addvid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // add video url
+            }
+        });
+
+        publishbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // publish post values to database.
+            }
+        });
     }
 }
