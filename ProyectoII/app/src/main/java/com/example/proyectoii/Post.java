6036ff1860a018069fa354f70d856635bf573da4
Alfredo Campos <joscaes12@gmail.com>
2019-11-03 22:58:29 -0600

    updated post usability

--- ProyectoII/app/src/main/java/com/example/proyectoii/Post.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/Post.java
@@ -2,12 +2,21 @@ package com.example.proyectoii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Post extends AppCompatActivity {
+
+    private static int IMAGE = 1;
+    private static int VIDEO = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,7 @@ public class Post extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // open gallery to select image
+                setLayout(IMAGE);
             }
         });
 
@@ -37,6 +47,7 @@ public class Post extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // add video url
+                setLayout(VIDEO);
             }
         });
 
@@ -44,7 +55,30 @@ public class Post extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // publish post values to database.
+                publish();
             }
         });
     }
+
+    public void publish() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void setLayout(int media) {
+        ImageView addIcon = findViewById(R.id.addimagen);
+        ImageView addVidIcon = findViewById(R.id.addvideourl);
+        EditText addLink = findViewById(R.id.ytlink);
+
+        if (media == IMAGE) {
+            addLink.setVisibility(View.INVISIBLE);
+            addIcon.setImageResource(R.drawable.image);
+            addVidIcon.setImageResource(R.drawable.videoplayerdisabled);
+        } else if (media == VIDEO) {
+            addLink.setVisibility(View.VISIBLE);
+            addIcon.setImageResource(R.drawable.imagedisabled);
+            addVidIcon.setImageResource(R.drawable.videoplayer);
+        }
+    }
+
 }
