8fd453bb8bed181386e393aba6ea8f0057697eb7
Alfredo Campos <joscaes12@gmail.com>
2019-11-09 16:29:47 -0600

    Search for users ready

--- ProyectoII/app/src/main/java/com/example/proyectoii/MenuFragments/SearchFragment.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/MenuFragments/SearchFragment.java
@@ -13,9 +13,13 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.proyectoii.Objetos.UserPreview;
 import com.example.proyectoii.Objetos.Usuario;
 import com.example.proyectoii.R;
+import com.example.proyectoii.Utils.RecyclerViewUserAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,21 +27,26 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class SearchFragment extends Fragment {
 
     private CheckBox users;
     private CheckBox posts;
     private EditText search;
+    private ArrayList<UserPreview> usersList;
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search,container,false);
+        view = inflater.inflate(R.layout.fragment_search,container,false);
 
         users = view.findViewById(R.id.userson);
         users.setChecked(true);
         posts = view.findViewById(R.id.postson);
         search = view.findViewById(R.id.criteriobusqueda);
+        usersList = new ArrayList<UserPreview>();
 
         users.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +80,7 @@ public class SearchFragment extends Fragment {
                 } else {
                     search("");
                 }
+                setAdapter();
             }
         });
 
@@ -90,6 +100,7 @@ public class SearchFragment extends Fragment {
     }
 
     public void search(String s) {
+        usersList.clear();
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
         Query query = databaseReference.child("usuarios").orderByChild("nombre")
                 .startAt(s)
@@ -100,7 +111,10 @@ public class SearchFragment extends Fragment {
 
                 if (dataSnapshot.hasChildren()) {
                     for (DataSnapshot dss : dataSnapshot.getChildren()) {
-                        Log.i("Usuario", dss.getValue().toString());
+                        String nombre = dss.child("nombre").getValue().toString() + " " + dss.child("apellido").getValue().toString();
+                        final UserPreview userPreview = new UserPreview(nombre, dss.child("id").getValue().toString());
+                        userPreview.setProfilepic(dss.child("linkImgPerfil").getValue().toString());
+                        usersList.add(userPreview);
                     }
                 }
             }
@@ -110,6 +124,13 @@ public class SearchFragment extends Fragment {
 
             }
         });
+    }
+
+    public void setAdapter() {
+        RecyclerView recyclerView = view.findViewById(R.id.resultados);
+        RecyclerViewUserAdapter userAdapter = new RecyclerViewUserAdapter(getContext(), usersList);
+        recyclerView.setAdapter(userAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
     }
 }
 
