dafe75a394c339b1b0c523319a50359e0fb4bc18
Diego Méndez Valverde <diegomvalverde@outlook.com>
2019-11-12 02:58:12 -0600

    Se agregan todas las opciones para edición de perfil

--- ProyectoII/app/src/main/java/com/example/proyectoii/MenuFragments/ProfileFragment.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/MenuFragments/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -45,6 +46,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import static com.example.proyectoii.MenuActivity.usuario;
 
@@ -53,12 +55,21 @@ public class ProfileFragment extends Fragment implements RecyclerViewPostAdapter
     FirebaseDatabase db = FirebaseDatabase.getInstance();
     DatabaseReference myRef = db.getReference();
     private View view;
+    private ImageView profileImage;
     public static int contadorPublicaciones ;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile,container,false);
+        profileImage = view.findViewById(R.id.profileImg);
+        if (!usuario.getLinkImgPerfil().equals(""))
+        {
+            profileImage.setImageDrawable(null);
+            Picasso.get()
+                    .load(usuario.getLinkImgPerfil())
+                    .into(profileImage);
+        }
         ArrayList<String> user = new ArrayList<>();
         user.add(usuario.getId());
         posts = new ArrayList<>();
--- ProyectoII/app/src/main/java/com/example/proyectoii/ProfileUtils/ProfileEditor.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/ProfileUtils/ProfileEditor.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.example.proyectoii.MenuActivity;
 import com.example.proyectoii.Objetos.Educacion;
 import com.example.proyectoii.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -20,9 +23,25 @@ public class ProfileEditor extends AppCompatActivity {
     EditText cityTxt;
     EditText mailTxt;
     EditText educationTxt;
+    DatePicker datePicker;
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = db.getReference();
+
+    public void cancelEdit(View view)
+    {
+        finish();
+    }
+
 
     public  void editProfile(View view)
     {
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth() + 1;
+        int year = datePicker.getYear();
+
+
+        MenuActivity.usuario.setFechaNac(day+"/"+month+"/"+year);
         MenuActivity.usuario.setApellido(lastnameTxt.getText().toString());
         MenuActivity.usuario.setNombre(nameTxt.getText().toString());
         MenuActivity.usuario.setCiudad(cityTxt.getText().toString());
@@ -42,7 +61,15 @@ public class ProfileEditor extends AppCompatActivity {
         }
         MenuActivity.usuario.setEducacicion(educacions);
 
+//        Saving info in firebase
+        myRef.child("usuarios").child(MenuActivity.usuario.getId()).child("ciudad").setValue(MenuActivity.usuario.getCiudad());
+        myRef.child("usuarios").child(MenuActivity.usuario.getId()).child("nombre").setValue(MenuActivity.usuario.getNombre());
+        myRef.child("usuarios").child(MenuActivity.usuario.getId()).child("apellido").setValue(MenuActivity.usuario.getApellido());
+        myRef.child("usuarios").child(MenuActivity.usuario.getId()).child("telefono").setValue(MenuActivity.usuario.getTelefono());
+        myRef.child("usuarios").child(MenuActivity.usuario.getId()).child("correo").setValue(MenuActivity.usuario.getCorreo());
+        myRef.child("usuarios").child(MenuActivity.usuario.getId()).child("fechaNac").setValue(MenuActivity.usuario.getFechaNac());
 
+        finish();
     }
 
     @Override
@@ -56,11 +83,15 @@ public class ProfileEditor extends AppCompatActivity {
         cityTxt = findViewById(R.id.cityTxt);
         mailTxt = findViewById(R.id.mailTxt);
         educationTxt = findViewById(R.id.educationTxt);
+        datePicker = findViewById(R.id.datePicker);
 
         nameTxt.setText(MenuActivity.usuario.getNombre());
         lastnameTxt.setText(MenuActivity.usuario.getApellido());
         contactTxt.setText(MenuActivity.usuario.getTelefono());
         cityTxt.setText(MenuActivity.usuario.getCiudad());
+        mailTxt.setText(MenuActivity.usuario.getCorreo());
+        String[] dateList = MenuActivity.usuario.getFechaNac().split("/");
+        datePicker.init(Integer.parseInt(dateList[2]), Integer.parseInt(dateList[1])-1, Integer.parseInt(dateList[0]), null);
 
     }
 }
