40e0f52aa42e98a455ddd68588c4e3667d0d11c5
Diego Méndez Valverde <diegomvalverde@outlook.com>
2019-11-13 00:27:02 -0600

    Lista la sección de solicitudes

--- ProyectoII/app/src/main/java/com/example/proyectoii/Utils/RecyclerViewFriendRequestAdapter.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/Utils/RecyclerViewFriendRequestAdapter.java
@@ -4,6 +4,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,9 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyectoii.MenuActivity;
 import com.example.proyectoii.Objetos.FriendRequest;
-import com.example.proyectoii.Objetos.PostObject;
 import com.example.proyectoii.Objetos.Usuario;
-import com.example.proyectoii.Post;
 import com.example.proyectoii.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,10 +20,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.List;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
@@ -91,13 +87,16 @@ public class RecyclerViewFriendRequestAdapter extends RecyclerView.Adapter<Recyc
     public void onBindViewHolder(@NonNull ViewHolderNotifications holder, final int position) {
         FriendRequest friendRequest = friendRequests.get(position);
         StringBuilder request = new StringBuilder();
+        Usuario usr = new Usuario();
         for (Usuario usuario: usuarios
              ) {
-            if (usuario.getId().equals(friendRequest.getFrom()))
-            request.append(usuario.getNombre()).append(" ").append(usuario.getApellido()).append(" quiere ser tu amigo");
+            if (usuario.getId().equals(friendRequest.getFrom())) {
+                request.append(usuario.getNombre()).append(" ").append(usuario.getApellido()).append(" quiere ser tu amigo");
+                usr = usuario;
+            }
         }
 
-        holder.asignarDatos(request.toString());
+        holder.asignarDatos(request.toString(),friendRequest, usr, MenuActivity.usuario );
 
     }
 
@@ -106,15 +105,65 @@ public class RecyclerViewFriendRequestAdapter extends RecyclerView.Adapter<Recyc
         return friendRequests.size();
     }
 
-    public class ViewHolderNotifications extends RecyclerView.ViewHolder {
+    public class ViewHolderNotifications extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView noti;
+        FriendRequest friendRequest;
+        Button accept, cancel;
+        Usuario from, me;
+
         public ViewHolderNotifications(@NonNull View itemView) {
             super(itemView);
             noti = itemView.findViewById(R.id.messageTxt);
+            accept = itemView.findViewById(R.id.acceptButton);
+            accept.setOnClickListener(this);
+            cancel = itemView.findViewById(R.id.cancelButton);
+            cancel.setOnClickListener(this);
         }
 
-        public void asignarDatos(String s) {
+        public void asignarDatos(String s,FriendRequest friendRequest, Usuario from, Usuario me) {
             noti.setText(s);
+            this.friendRequest = friendRequest;
+            this.from = from;
+            this.me = me;
+
+            if (!friendRequest.isPending())
+            {
+                accept.setEnabled(false);
+                cancel.setEnabled(false);
+            }
+        }
+
+
+
+
+        @Override
+        public void onClick(View v) {
+            accept.setEnabled(false);
+            cancel.setEnabled(false);
+            switch (v.getId())
+            {
+                case R.id.acceptButton:
+                    myRef.child("solicitudes").child(friendRequest.getIdRequest()).child("accepted").setValue(true);
+                    myRef.child("solicitudes").child(friendRequest.getIdRequest()).child("pending").setValue(false);
+
+//                    Update Friends
+                    ArrayList<String> theirFriends = from.getAmigos();
+                    theirFriends.add(me.getId());
+
+                    ArrayList<String> myFriends = me.getAmigos();
+                    myFriends.add(from.getId());
+
+                    myRef.child("usuarios").child(from.getId()).child("amigos").setValue(theirFriends);
+                    myRef.child("usuarios").child(me.getId()).child("amigos").setValue(myFriends);
+
+                    break;
+
+                case R.id.layout:
+                    myRef.child("solicitudes").child(friendRequest.getIdRequest()).child("accepted").setValue(false);
+                    myRef.child("solicitudes").child(friendRequest.getIdRequest()).child("pending").setValue(false);
+                    break;
+            }
         }
     }
+
 }
