9cbadbe2403a2b7ac3cad8bbcac8120172142011
Alfredo Campos <joscaes12@gmail.com>
2019-11-13 02:01:25 -0600

    added profile layout

--- ProyectoII/app/src/main/java/com/example/proyectoii/ProfileView.java
+++ ProyectoII/app/src/main/java/com/example/proyectoii/ProfileView.java
@@ -1,18 +1,310 @@
 package com.example.proyectoii;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProfileView extends AppCompatActivity {
+import com.amrdeveloper.reactbutton.ReactButton;
+import com.amrdeveloper.reactbutton.Reaction;
+import com.example.proyectoii.Objetos.Comentario;
+import com.example.proyectoii.Objetos.Educacion;
+import com.example.proyectoii.Objetos.PostObject;
+import com.example.proyectoii.Objetos.PostWithUser;
+import com.example.proyectoii.Objetos.Reaccion;
+import com.example.proyectoii.Objetos.Usuario;
+import com.example.proyectoii.Utils.RecyclerViewPostAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-    private String userId;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProfileView extends AppCompatActivity implements RecyclerViewPostAdapter.OnPostListener {
+
+    private ArrayList<PostWithUser> posts;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = db.getReference();
+    private View view;
+    private ImageView profileImage;
+    public static int contadorPublicaciones ;
+    private Usuario usuario;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
 
-        userId = getIntent().getStringExtra("USERID");
+        String userid = getIntent().getStringExtra("USERID");
+        //set user
+
+        profileImage = findViewById(R.id.profileImg);
+        if (!usuario.getLinkImgPerfil().equals(""))
+        {
+            profileImage.setImageDrawable(null);
+            Picasso.get()
+                    .load(usuario.getLinkImgPerfil())
+                    .into(profileImage);
+        }
+        ArrayList<String> user = new ArrayList<>();
+        user.add(usuario.getId());
+        posts = new ArrayList<>();
+        obtenerPost(user);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Post.class);
+                startActivity(intent);
+            }
+        });
+
+
+        TextView textView = findViewById(R.id.nameTxt);
+        textView.setText(String.format("%s %s", usuario.getNombre(), usuario.getApellido()));
+
+        StringBuilder userInfo = new StringBuilder(String.format("%s %s", "Fecha de nacimiento:", usuario.getFechaNac()));
+        userInfo.append("\n");
+        userInfo.append(String.format("%s %s", "Ciudad:", usuario.getCiudad()));
+        userInfo.append("\n");
+        userInfo.append(String.format("%s %s", "Contacto:", usuario.getTelefono()));
+        userInfo.append("\n");
+        userInfo.append(String.format("%s %s", "Correo  de contacto:", usuario.getCorreo()));
+
+        if (usuario.getEducacicion() != null && usuario.getEducacicion().size() > 0)
+            for (Educacion education: usuario.getEducacicion())
+            {
+                userInfo.append("\n");
+                userInfo.append(String.format("%s %s", String.format("%s:", education.getTipoInstitucion()), education.getNombreInstitucion()));
+            }
+        TextView infoTxt = findViewById(R.id.infoTxt);
+
+        infoTxt.setText(userInfo.toString());
+
+
+        Button viewGallery = view.findViewById(R.id.btn_gallery);
+        viewGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Gallery.class);
+                intent.putExtra("USERID", usuario.getId());
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        contadorPublicaciones = -1;
+    }
+
+    public void obtenerPost(final ArrayList<String> amigos){
+
+        Query query = myRef.orderByChild("posts");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<DataSnapshot> dataSnapshots = new ArrayList<>();
+                for(DataSnapshot singleSnapshot: dataSnapshot.child("posts").getChildren()){
+                    dataSnapshots.add(singleSnapshot);
+                }
+
+                if(contadorPublicaciones == -1){
+                    contadorPublicaciones = dataSnapshots.size();
+                    posts = new ArrayList<>();
+                }
+
+                int postEncontrados = 0;
+                while(contadorPublicaciones > 0 && postEncontrados < 10){
+                    DataSnapshot singleSnapshot = dataSnapshots.get(contadorPublicaciones-1);
+                    PostObject postObject = singleSnapshot.getValue(PostObject.class);
+
+                    String auhotID = postObject.getAuthorId();
+
+                    if(amigos.contains(auhotID)){
+                        Usuario usuario = dataSnapshot.child("usuarios").child(postObject.getAuthorId()).getValue(Usuario.class);
+
+                        String username = usuario.getNombre() + " " + usuario.getApellido();
+                        PostWithUser post = new PostWithUser(usuario.getId(),postObject.getDescripcion(),postObject.getTipo(),postObject.getIdPost(),username,usuario.getLinkImgPerfil());
+
+                        for(DataSnapshot reactionSnapshot: singleSnapshot.child("reacciones").getChildren()){
+                            String autorReaccion = reactionSnapshot.child("idAutor").getValue(String.class);
+                            int tipoReaccion = reactionSnapshot.child("tipoReaccion").getValue(Integer.class);
+
+                            post.getReacciones().add(new Reaccion(autorReaccion,tipoReaccion));
+                        }
+
+                        for(DataSnapshot commentSnapshot: singleSnapshot.child("comentarios").getChildren()){
+                            post.getComentarios().add(commentSnapshot.getValue(Comentario.class));
+                        }
+
+                        switch (post.getTipo()){
+                            case "IMAGE":
+                                post.setImageURI(singleSnapshot.child("imageURI").getValue(String.class));
+                                break;
+                            case "VIDEO":
+                                post.setVideoUrl(singleSnapshot.child("videoUrl").getValue(String.class));
+                                break;
+                        }
+
+                        post.setFecha(singleSnapshot.child("fecha").getValue(String.class));
+                        posts.add(post);
+
+                        postEncontrados++;
+
+                    }
+
+                    contadorPublicaciones--;
+
+                }
+
+                iniciarRecyclerView();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onContentClick(PostWithUser postWithUser) {
+        Intent intent = new Intent(this, VerPublicacionActivity.class);
+        intent.putExtra("Post",postWithUser);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onTextClick(PostWithUser postWithUser) {
+        Intent intent = new Intent(this, VerPublicacionActivity.class);
+        intent.putExtra("Post",postWithUser);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onLikeClick(PostWithUser postWithUser, ReactButton reactButton) {
+
+        Reaccion reaccion = new Reaccion(MenuActivity.usuario.getId(),0);
+        if(postWithUser.getReacciones().contains(reaccion)){
+            int reactionIndex = postWithUser.getReacciones().indexOf(reaccion);
+            agregarReaccion(reaccion,reactionIndex,postWithUser);
+            reactButton.setCurrentReaction(reactButton.getDefaultReaction());
+        }
+        else{
+            reactButton.setCurrentReaction(RecyclerViewPostAdapter.reactions[1]);
+            reaccion.setTipoReaccion(1);
+            postWithUser.getReacciones().add(reaccion);
+            agregarReaccion(reaccion,postWithUser.getReacciones().size()-1,postWithUser);
+        }
+
+    }
+
+    @Override
+    public void onLikeLongClick(PostWithUser postWithUser, Reaction reaction) {
+        int tipoReaccion;
+        switch (reaction.getReactType()){
+            case "Me gusta":
+                tipoReaccion = 1;
+                break;
+            case "Me encanta":
+                tipoReaccion = 2;
+                break;
+            case "Me divierte":
+                tipoReaccion = 3;
+                break;
+            case "Me asombra":
+                tipoReaccion = 4;
+                break;
+            case "No me gusta":
+                tipoReaccion = 5;
+                break;
+            case "Me enoja":
+                tipoReaccion = 6;
+                break;
+            default:
+                tipoReaccion = 0;
+                break;
+        }
+        Reaccion reaccion = new Reaccion(MenuActivity.usuario.getId(),tipoReaccion);
+        if(postWithUser.getReacciones().contains(reaccion)){
+            int reactionIndex = postWithUser.getReacciones().indexOf(reaccion);
+            postWithUser.getReacciones().set(reactionIndex,reaccion);
+            agregarReaccion(reaccion,reactionIndex,postWithUser);
+
+
+        }
+
+        else{
+            postWithUser.getReacciones().add(reaccion);
+            agregarReaccion(reaccion,postWithUser.getReacciones().size()-1,postWithUser);
+
+        }
+    }
+
+    @Override
+    public void onCommentClick(PostWithUser postWithUser) {
+        Intent intent = new Intent(this, VerPublicacionActivity.class);
+        intent.putExtra("Post",postWithUser);
+        intent.putExtra("Comentario",true);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onProfileClick(String idUser) {
+        Toast.makeText(this, "Abrir perfil de " + idUser, Toast.LENGTH_SHORT).show();
+    }
+
+    public void agregarReaccion(Reaccion reaccion,int reaccionIndex,PostWithUser post){
+        final FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef  = mFirebaseDatabase.getReference();
+        if (reaccion.getTipoReaccion() != 0) {
+            myRef.child("posts")
+                    .child(post.getIdPost())
+                    .child("reacciones")
+                    .child(String.valueOf(reaccionIndex))
+                    .setValue(reaccion);
+        }
+        else{
+            myRef.child("posts")
+                    .child(post.getIdPost())
+                    .child("reacciones")
+                    .child(String.valueOf(reaccionIndex))
+                    .setValue(null);
+            post.getReacciones().remove(reaccionIndex);
+        }
+
+
+    }
+
+    public void iniciarRecyclerView(){
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        RecyclerViewPostAdapter adapter = new RecyclerViewPostAdapter(getApplicationContext(),posts,this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
