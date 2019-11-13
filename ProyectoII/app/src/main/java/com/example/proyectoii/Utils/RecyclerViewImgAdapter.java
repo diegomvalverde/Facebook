e4b9c3c6a3f91a90648dc65d289cddf6c87ba23e
Alfredo Campos <joscaes12@gmail.com>
2019-11-12 23:25:50 -0600

    added image gallery

---
+++ ProyectoII/app/src/main/java/com/example/proyectoii/Utils/RecyclerViewImgAdapter.java
@@ -0,0 +1,65 @@
+package com.example.proyectoii.Utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.proyectoii.Objetos.UserPreview;
+import com.example.proyectoii.ProfileView;
+import com.example.proyectoii.R;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewImgAdapter extends RecyclerView.Adapter<RecyclerViewImgAdapter.ViewHolder> {
+
+    private ArrayList<String> usersImgs;
+    private Context mContext;
+
+    public RecyclerViewImgAdapter(Context context, ArrayList<String> imgs) {
+        mContext = context;
+        usersImgs = imgs;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewImgAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_img_gallery,parent,false);
+        RecyclerViewImgAdapter.ViewHolder holder = new RecyclerViewImgAdapter.ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final RecyclerViewImgAdapter.ViewHolder holder, final int position) {
+        String imageLink = usersImgs.get(position);
+        if (!imageLink.isEmpty()) {
+            Uri muri = Uri.parse(imageLink);
+            Glide.with(mContext).load(muri).into(holder.image);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return usersImgs.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.imgview);
+        }
+    }
+}
---
+++ ProyectoII/app/src/main/java/com/example/proyectoii/gallery.java
@@ -0,0 +1,68 @@
+package com.example.proyectoii;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.proyectoii.Objetos.PostObject;
+import com.example.proyectoii.Utils.RecyclerViewImgAdapter;
+import com.example.proyectoii.Utils.RecyclerViewPostAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class gallery extends AppCompatActivity {
+
+    private String userid;
+    private ArrayList<String> userImgs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        userid = getIntent().getStringExtra("USERID");
+        userImgs = new ArrayList<String>();
+        getUserImgs();
+        setRecyclerView();
+    }
+
+    public void getUserImgs() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        Query query = ref.orderByChild("posts");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dss: dataSnapshot.getChildren()) {
+                    PostObject post = dss.getValue(PostObject.class);
+                    if (post.getAuthorId().equals(userid)) {
+                        if (post.getTipo().equals("IMAGE")){
+                            userImgs.add(post.getImageURI());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void setRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.gallery_recycler);
+        RecyclerViewImgAdapter adapter = new RecyclerViewImgAdapter(getApplicationContext() ,userImgs);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+}
